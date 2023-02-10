412d23c2e612b6111332d7405bf7b0104247de5a
hasakiXie123 <76777406+hasakiXie123@users.noreply.github.com>
2023-02-10 16:03:42 +0800

    Update pytorch.py

--- pytorch.py
+++ pytorch.py
@@ -3,3 +3,10 @@ import torch
 ## turn a label vector into a one_hot label vector
 a = torch.tensor([0, 1, 0, 1]).resize(4, 1)
 b = torch.nn.functional.one_hot(a).squeeze(dim=1)# [4, 1, 2] into [4, 2]
+
+## replace the empty row of a vector with something else
+zeros = torch.zeros([4, 2048])
+one = torch.ones([1, 2048])
+non_idxs = torch.all(zeros[..., :] == 0, axis=1).nonzero().tolist()
+non_idxs = [x[0] for x in non_idxs]
+zeros[non_idxs] = one
